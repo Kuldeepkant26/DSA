996e61582248f899a09df90052379eb9d05ff549
Kuldeepkant26 <picasuplayers@gmail.com>
2024-06-09 19:08:00 +0530

    DFS completed

---
+++ BST/Search7.js
@@ -0,0 +1,104 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Bst {
+    constructor() {
+        this.root = null;
+    }
+    isEmpty() {
+        return this.root === null;
+    }
+    insert(data) {
+        let newNode = new Node(data);
+        if (this.isEmpty()) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+    insertNode(root, newNode) {
+        if (root.data > newNode.data) {
+            if (root.left === null) {
+                root.left = newNode;
+            } else {
+                this.insertNode(root.left, newNode);
+            }
+        } else {
+            if (root.right === null) {
+                root.right = newNode;
+            } else {
+                this.insertNode(root.right, newNode);
+            }
+        }
+    }
+    search(key) {
+        if (this.isEmpty()) {
+            console.log("tree is Empty");
+            return false;
+        } else {
+            return this.findNode(this.root, key);
+        }
+    }
+    findNode(root, key) {
+        if (root === null) {
+            return false;
+        }
+        if (root.data === key) {
+            return true;
+        }
+        if (root.data > key) {
+            this.findNode(root.left, key);
+        } else {
+            this.findNode(root.right, key);
+        }
+    }
+    preorder() {
+        this.preorderTraversal(this.root);
+    }
+    preorderTraversal(root) {
+        if (root != null) {
+            console.log(root.data);
+            this.preorderTraversal(root.left);
+            this.preorderTraversal(root.right);
+        }
+    }
+    inorder() {
+        this.inorderTraversal(this.root)
+    }
+    inorderTraversal(root) {
+        if (root != null) {
+            this.inorderTraversal(root.left);
+            console.log(root.data);
+            this.inorderTraversal(root.right);
+        }
+    }
+    postorder(root) {
+        this.postorderTraversal(this.root);
+    }
+    postorderTraversal(root) {
+        if (root != null) {
+            this.postorderTraversal(root.left);
+            this.postorderTraversal(root.right);
+            console.log(root.data);
+        }
+    }
+
+}
+
+let b1 = new Bst();
+b1.insert(1)
+b1.insert(2)
+b1.insert(3)
+b1.insert(4)
+
+
+b1.inorder();
+console.log(b1.search(1));
+b1.preorder();
+console.log(b1.search(1));
+b1.postorder();
